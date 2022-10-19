087b2076851e89396b3e67de2df3b02a8ad8b394
Bhavin <you@example.com>
2022-10-19 19:36:50 -0400

    initial commit

---
+++ expenses/views.py
@@ -0,0 +1,8 @@
+from winreg import REG_WHOLE_HIVE_VOLATILE
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'expenses/index.html')
+def add_expense(request):
+    return render(request, 'expenses/add_expenses.html')
